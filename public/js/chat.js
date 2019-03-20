23e834cd1fe193026d5e606968b7c2f8a662a7f0
Robert Checco <robert.checco@gmail.com>
2019-03-20 16:57:27 -0400

    Successfully emit welcome message to render in dom of all clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,10 @@ socket.on('countUpdated', (count) => {
     console.log('The count has been updated!', count);
 });
 
+socket.on('message', (message) => {
+    const button = document.querySelector('h1').innerHTML = message;
+})
+
 const button = document.querySelector('#increment').addEventListener('click', () => {
     console.log('Clicked');
     socket.emit('increment');
--- src/index.js
+++ src/index.js
@@ -10,21 +10,33 @@ const io = socketio(server);
 const port = process.env.Port || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+// Goal: Send a welcome message to new users
 
+// 1. Have server emit "message when new client connects"
+    // - Send "Welcome!" as the event data
+
+const message = "Welcome!"
 io.on('connection', (socket) => {
-    console.log('New Websocket connection');
-    socket.emit('countUpdated', count);
-
-    socket.on('increment', () => {
-        count++;
-        // socket.emit('countUpdated', count);
-        io.emit('countUpdated', count);
-    })
+    socket.emit('message', message);
 })
+// 2. Have client listen for message event and print to console.
+// 3. Test the works
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
+
+// let count = 0;
+
+// io.on('connection', (socket) => {
+//     console.log('New Websocket connection');
+//     socket.emit('countUpdated', count);
+
+//     socket.on('increment', () => {
+//         count++;
+//         // socket.emit('countUpdated', count);
+//         io.emit('countUpdated', count);
+//     })
+// })
