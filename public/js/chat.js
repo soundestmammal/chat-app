36a8cec72422f365acce6cf6c3a481d9c751fd3e
Robert Checco <robert.checco@gmail.com>
2019-03-21 16:32:23 -0400

    Can send messages and render messages to all clients in the dom

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,18 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
+socket.on('chat message', (message) => {
+    var node = document.createElement('LI');
+    var textnode = document.createTextNode(message);
+    node.appendChild(textnode);
+    document.getElementById("message-list").appendChild(node);
 });
 
-socket.on('message', (message) => {
-    document.querySelector('h1').innerHTML = message;
-})
-
 document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const message = e.target.elements.message.value
-
-    socket.emit('sendMessage', message);
+    e.preventDefault(); // prevent the reload of the browser
+    const message = e.target.elements.message.value;
+    socket.emit('chat message', message);
+    document.querySelector('#message-input').value = "";
+    return false;
 });
 
 
--- src/index.js
+++ src/index.js
@@ -12,14 +12,39 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
+// This is like the global scope for the web socket
 io.on('connection', (socket) => {
-    socket.emit('message', "Welcome!");
+    // Disconnect Functionality
+    socket.on('disconnect', () => {
+        console.log("A user disconnected.")
+    });
 
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message);
+    socket.on('chat message', (message) => {
+        console.log(`Message: ${message}`);
+        io.emit('chat message', message);
     });
 });
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
+
+// I need to be more structured. What is the plan? Think 1. 2. 3. 
+
+/* 
+The server needs to handle:
+
+1) When a user connects/disconnects from the server we want to log that.
+2) When a user sends a message to the server ->
+    Listen for the event
+    io.emit the event to all connections
+
+
+
+
+
+Connection
+    Disconnection
+    chat message
+
+*/
