0b23e5e068c83e6b49edb5e8b0c9eb9c5b4cf11a
Robert Checco <robert.checco@gmail.com>
2019-03-21 20:05:36 -0400

    Hook up twit to the application to live stream tweets

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,11 @@
 const socket = io();
 
-socket.on('chat message', (message) => {
+socket.on('chat message', (text) => {
     var node = document.createElement('LI');
-    var textnode = document.createTextNode(message);
+    var textnode = document.createTextNode(text);
     node.appendChild(textnode);
     document.getElementById("message-list").appendChild(node);
+    console.log(text);
 });
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
@@ -15,6 +16,33 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     return false;
 });
 
+// I want to create a resusable dom element that I am going to inject the information that I need.
+
+
+
+var render = function(template, node) {
+    // Put something here
+}
+
+
+
+
+
+const twitterTemplate = {
+    constructor() {
+        this.photo = photo,
+        this.text = text,
+        this.time = time
+    }
+<li>
+    <div class="twitterTemplate">
+        <div>Photo</div>
+        <div>Text</div>
+        <div>Time</div>
+    </div>
+</li>
+}
+// Everytime I get a chat message, I want to pass the information to this element and render it.
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Twit = require('twit');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,15 @@ const port = process.env.Port || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+
+const T = new Twit({
+    consumer_key:         'JDkYuuyxW16aXOPpyiiqAIF2O',
+    consumer_secret:      'MHePNqpCx0Kgp8HBSkjQ6oE1lmFRAtcV9bs603EKzH6veZczLW',
+    access_token:         '61657873-ngpFtj8OB0Qf0fxe6I2DritCcF1YOce67BxVlCWzU',
+    access_token_secret:  '9sdvGTcgxClappJLHY80GIPoV4gsHY6zhlcOvhy3cuPUZ',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+});
 
 // This is like the global scope for the web socket
 io.on('connection', (socket) => {
@@ -22,6 +32,13 @@ io.on('connection', (socket) => {
     socket.on('chat message', (message) => {
         console.log(`Message: ${message}`);
         io.emit('chat message', message);
+    });
+
+    const stream = T.stream('statuses/filter', { track: 'waymo, gm, cruise automation, nuro, nuroai' });
+    stream.on('tweet', function (tweet) {
+        var { create_at, text, user } = tweet;
+        console.log(tweet);
+        io.emit('chat message', create_at, text, user);
     });
 });
 
