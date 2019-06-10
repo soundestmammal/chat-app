c6f98f2bf966f18704b15e309cf82b2e71d8f3b7
Robert Checco <robert.checco@gmail.com>
2019-06-10 13:39:51 -0400

    Load in CDNs

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,16 +3,28 @@ const socket = io();
 // server (emit) -> client (receive) -- acknowledgement --> server
 // client (emit) -> server (receive) -- acknowledgement --> client
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
     console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    $messageFormButton.setAttribute('disabled', 'disabled');
+    // disable
     const message = e.target.elements.message.value;
 
     socket.emit('sendMessage', message, (error) => {
+        // enable
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if (error) {
             return console.log(error);
         }
@@ -21,7 +33,9 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     });
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
+    // disable button here
+    $sendLocationButton.setAttribute('disabled', 'disabled');
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser');
     }
@@ -31,6 +45,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (error) => {
+            // enable
+            $sendLocationButton.removeAttribute('disabled');
             if (error) {
                 return console.log(error);
             }
