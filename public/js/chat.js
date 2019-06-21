82c67f5e81b3571ebc953905f11a2e825959db4e
Robert Checco <robert.checco@gmail.com>
2019-06-21 16:44:04 -0400

    Render the Chat Room title and users in the room

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,6 +13,7 @@ const $messages = document.querySelector('#messages');
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationTemplate = document.querySelector('#location-message-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }); // Eliminates "?" from the string
@@ -36,8 +37,11 @@ socket.on('locationMessage', (message) => {
 });
 
 socket.on('roomData', ({ room, users }) => {
-    console.log(room);
-    console.log(users);
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html;
 })
 
 $messageForm.addEventListener('submit', (e) => {
