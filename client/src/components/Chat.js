69c60f5fb5c3c38a3c97ec339ef378285d177853
Robert Checco <robert.checco@gmail.com>
2019-11-25 18:53:50 -0500

    Add state to the Chat component
    Eventually refactor this to use redux

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import Message from './Message';
+import "../styles/chat.css";
 
 class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: {},
+            input: "",
+        }
+    }
+
     render() {
         return(
             <div className="chat">
-                <div class="chat__sidebar" id="sidebar">
+                <div className="chat__sidebar" id="sidebar">
 
                 </div>
                 <div className="chat__main">
                     <div id="messages" className="chat__messages">
-                    
+                        <Message />
                     </div>
                     <div className="compose">
                         <form id="message-form">
-                            <input name="message" placeholder="Message" required autocomplete="off" />
+                            <input name="message" placeholder="Message" required autocomplete="off"/>
                             <button>Send</button>
                         </form>
                     </div>
