9e313308ef79b7fceb04a60b620cbbd4703bd712
Robert Checco <robert.checco@gmail.com>
2019-12-04 18:20:37 -0500

    Code for making get requests to the server

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Login from '../src/components/Login';
 import "../src/styles/App.css";
 
-function App() {
-  return (
-    <div className="container">
-      <Login />
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    // call our fetch function below once the component mounts
+    this.callBackendAPI()
+    .then(res => this.setState({ data: res.age }))
+    .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/fetchUsers');
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <Login />
+        <p>This is where the data will be: {this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
