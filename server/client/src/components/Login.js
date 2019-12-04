be6248f0e8ed60aff1c67681b1c5825a6b4b49ad
Robert Checco <robert.checco@gmail.com>
2019-12-04 18:26:08 -0500

    Make the two input fields controlled components

--- server/client/src/components/Login.js
+++ server/client/src/components/Login.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import "../styles/Login.css";
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayName: "",
+            chatRoomName: "",
+        }
+    }
+
+    handleNameChange = (e) => {
+        this.setState({displayName: e.target.value});
+        console.log(this.state.displayName);
+    }
+
+    handleChatChange = (e) => {
+        this.setState({chatRoomName: e.target.value});
+        console.log(this.state.chatRoomName);
+    }
+
+    handleSubmit = (e) => {
+        alert(`A message was submitted:  ${this.state.value}`);
+        this.generateMessageObject();
+        e.preventDefault();
+        this.setState({ value: "" });
+    }
+
     render() {
         return (
             <div className="centered-form">
                 <div className="centered-form__box">
                     <form action="/chat.html">
                         <label>Display Name</label>
-                        <input type="text" name="username" placeholder="Display name" required autocomplete="false" />
+                        <input type="text" name="username" placeholder="Display name" required autoComplete="off" value={ this.state.chatRoomName } onChange={ this.handleChatChange } />
                         <label>Room</label>
-                        <input type="text" name="room" placeholder="Room" required autocomplete="false" />
+                        <input type="text" name="room" placeholder="Room" required autoComplete="off" />
                         <button>Join</button>
                     </form>  
                 </div>       
