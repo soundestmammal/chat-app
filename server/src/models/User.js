9f7ac939911c69ebf6601627b88765b7efbfe9f1
Robert Checco <robert.checco@gmail.com>
2019-12-19 14:10:57 -0500

    New user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const { Schema }  = mongoose;
 
-const userSchema = new Schema({
-    name: String,
-    age: Number,
-});
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+    }
+);
 
 const User = new mongoose.model('User', userSchema);
 
